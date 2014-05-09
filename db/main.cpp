ebb16a0f007ce2d4ce384435753f497b5153eb2b
berak <px1704@web.de>
2014-05-09 12:05:14 +0200

    db: cleanup

--- db/main.cpp
+++ db/main.cpp
@@ -20,7 +20,7 @@ int main()
     //Ptr<opencv_db> db = createPostgresDb();//10099);
     //bool ok = db->open("p4p4","localhost","postgres","p1p2p3p4");
     Ptr<opencv_db> db = createRedisDb(6379);//10099);
-    bool ok = db->open("0","gateway-1.simpleredis.com","paula","i8418d75a28c360a38c8d886b2c62410c56452890z");
+    bool ok = db->open("0","gateway-1.simpleredis.com","paula","ib58a1c19e9c3b4c6c6c775d1d9887f9fb52fb57cz");
     //Ptr<opencv_db> db = createSqlite3Db();
     //bool ok = db->open("ocv.sqlite",0,0,0);
     //Ptr<opencv_db> db = createFsDb();
@@ -38,19 +38,18 @@ int main()
     ////m = imread("../../demo/lena.jpg",1);
     ////ok = db->write(table,"lena",m);
 
-    //vector<int> prm; prm.push_back(IMWRITE_PNG_COMPRESSION); prm.push_back(2);
-    //vector<uchar> mb; imencode(".png",m,mb,prm);
-    //ok = db->write(table,"tuna.png",Mat(mb));
+    vector<int> prm; prm.push_back(IMWRITE_PNG_COMPRESSION); prm.push_back(2);
+    vector<uchar> mb; imencode(".png",m,mb,prm);
+    ok = db->write(table,"tuna.png",Mat(mb));
 
     Mat md = Mat::eye(3,4,CV_64F);
     ok = db->write(table,"eye",md);
     cerr << "2 " << ok << endl;
-    if ( ! ok ) return 2; 
     
     Mat m2;
     ok = db->read(table,"eye",m2);
     cerr << "3 " << ok << endl;
-    if ( ! ok ) return 3;
+
     if ( ok && m2.rows )
         imshow("success!",m2), waitKey();
     
@@ -60,7 +59,7 @@ int main()
     //
     ok = db->read(table,"tuna.png",m2);
     cerr << "4 " << ok << endl;
-    if ( ! ok ) return 4;
+
     if ( ok && m2.rows ) {
         m2 = imdecode(m2,1);
         if ( ok && m2.rows )
