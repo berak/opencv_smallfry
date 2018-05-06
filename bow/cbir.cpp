bf057af8020cadc7a968345b2311c64e89bbeb87
berak <px1704@web.de>
2018-05-06 21:22:33 +0200

    rootsift

--- bow/cbir.cpp
+++ bow/cbir.cpp
@@ -1,4 +1,6 @@
 #include "opencv2/opencv.hpp"
+#include "opencv2/core/ocl.hpp"
+#include <opencv2/core/utils/filesystem.hpp>
 #include "opencv2/xfeatures2d.hpp"
 
 #include "BowKMajorityTrainer.h"
@@ -25,7 +27,8 @@ static void trainFloat(Ptr<DescriptorMatcher> &matcher, Ptr<BOWTrainer> &bow_tra
 static void trainFeatures(const String &fname, Ptr<Feature2D> &det, Ptr<Feature2D> &ext, Ptr<DescriptorMatcher> &matcher, Ptr<BOWTrainer> &bow_train, int nclusters) {
    if (fname=="surf")    { det = ext = xfeatures2d::SURF::create(); trainFloat(matcher, bow_train, nclusters); }
    if (fname=="sift")    { det = ext = xfeatures2d::SIFT::create(); trainFloat(matcher, bow_train, nclusters); }
-   if (fname=="vlad")    { det = ext = xfeatures2d::SURF::create(); trainFloat(matcher, bow_train, nclusters); }
+   if (fname=="vlad")    { det = ext = xfeatures2d::SURF::create(100,4,3,false,true); trainFloat(matcher, bow_train, nclusters); }
+   if (fname=="vladsift"){ det = ext = xfeatures2d::SIFT::create(); trainFloat(matcher, bow_train, nclusters); }
    if (fname=="latch")   { det = ORB::create(); ext = xfeatures2d::LATCH::create(); trainBinary(matcher, bow_train, nclusters); }
    if (fname=="lucid")   { det = ORB::create(); ext = xfeatures2d::LUCID::create(3); trainBinary(matcher, bow_train, nclusters); }
    if (fname=="vgg")     { det = xfeatures2d::SURF::create(); ext = xfeatures2d::VGG::create(); trainFloat(matcher, bow_train, nclusters); }
@@ -33,6 +36,23 @@ static void trainFeatures(const String &fname, Ptr<Feature2D> &det, Ptr<Feature2
    if (fname=="orb")     { det = ext = ORB::create(); trainBinary(matcher, bow_train, nclusters); }
    if (fname=="brisk")   { det = ORB::create(); ext = BRISK::create(); trainBinary(matcher, bow_train, nclusters); }
    if (fname=="brief")   { det = ORB::create(); ext = xfeatures2d::BriefDescriptorExtractor::create(); trainBinary(matcher, bow_train, nclusters); }
+}
+
+static Mat rootsift(Mat &feat) {
+    PROFILE
+    {
+        PROFILEX("root:norm")
+        norm(feat,feat,NORM_L1);
+    }
+    {
+        PROFILEX("root:sqrt")
+        sqrt(feat,feat);
+    }
+    { // for SURF (has neg numbers)
+        PROFILEX("root:patch")
+        patchNaNs(feat, 0);
+    }
+    return feat;
 }
 
 static Mat getImg(String fn, String fname) {
@@ -46,6 +66,7 @@ static Mat getImg(String fn, String fname) {
 }
 
 static Mat sign(const Mat &m) {
+    PROFILE
     Mat s(m.size(),m.type(),1.0f);
     s.setTo(-1, m<0);
     return s;
@@ -59,24 +80,31 @@ static Mat vlad_feature(Ptr<Feature2D> f2d,  Ptr<DescriptorMatcher> matcher, con
         PROFILEX("vlad:detect")
         f2d->detectAndCompute(img, Mat(), kp, desc);
     }
-    if (kp.size()) {
+    if (desc.rows>0) {
         PROFILEX("vlad:compute")
+        rootsift(desc);
+
         vector<DMatch> matches;
         matcher->match(desc, vocab, matches);
         feat = Mat(vocab.size(), CV_32F, 0.0f);
-        for (size_t j=0; j<matches.size(); j++) {
-            Mat dr = desc.row(matches[j].queryIdx);
-            Mat vr = vocab.row(matches[j].trainIdx);
-            Mat re = vr - dr;
-            normalize(re,re);
-            feat.row(matches[j].trainIdx) += re / vocab.rows;
+        {
+            PROFILEX("vlad:vlad")
+            for (size_t j=0; j<matches.size(); j++) {
+                Mat dr = desc.row(matches[j].queryIdx);
+                Mat vr = vocab.row(matches[j].trainIdx);
+                Mat re = vr - dr;
+                normalize(re,re); // innorm
+                feat.row(matches[j].trainIdx) += re / vocab.rows;
+            }
         }
-        // power normalization
-        Mat f2;
-        sqrt(abs(feat), f2);
-        feat = sign(feat).mul(f2);
-        // L2 normalization
-        normalize(feat,feat);
+        { PROFILEX("vlad:post")
+            // power normalization
+            Mat f2;
+            sqrt(abs(feat), f2);
+            feat = sign(feat).mul(f2);
+            // L2 normalization
+            normalize(feat,feat);
+        }
         return feat.reshape(1,1);
     }
     return Mat();
@@ -104,13 +132,22 @@ static cv::Ptr<cv::flann::Index> train_index(const Mat &trainData) {
     return makePtr<cv::flann::Index>(trainData, cv::flann::LinearIndexParams(), cvflann::FLANN_DIST_L2);
 }
 
+string category(string s, int off) {
+    string s2 = s.substr(off);
+    int se = s2.find_last_of('\\');
+    if (se < 0)
+        se = s2.find_last_of('/');
+    return s2.substr(0,se);
+}
+
 int main( int argc, char ** argv ) {
-    const char *datapath = "c:/data/caltech/101_ObjectCategories";
+    const String datapath = "c:/data/caltech/101_ObjectCategories";
     //const char *datapath = "c:/data/faces/att";
     String fname  = argc>1 ? argv[1] : "vlad";
     int nclusters = argc>2 ? atoi(argv[2]) : 64;
-    int ncimages  = argc>3 ? atoi(argv[3]) : 1000;
-
+    int ncimages  = argc>3 ? atoi(argv[3]) : 30;
+    theRNG().state = getTickCount();
+    ocl::setUseOpenCL(false);
     Ptr<Feature2D> det,ext;
     Ptr<DescriptorMatcher> matcher;
     Ptr<BOWTrainer> bow_train;
@@ -128,66 +165,90 @@ int main( int argc, char ** argv ) {
     if (!fs.isOpened()) {
         int fsize=0;
         for (int i=0; i<ncimages; i++) {
+            PROFILEX("add:cluster")
             int id = theRNG().uniform(0,fn.size());
-            std::vector<KeyPoint> kp;
             Mat img = getImg(fn[id], fname);
             if (img.empty()) continue;
+
             Mat feat;
+            std::vector<KeyPoint> kp;
             if (det == ext) {
                 det->detectAndCompute(img, Mat(), kp, feat);
             } else {
                 det->detect(img, kp);
                 ext->compute(img, kp, feat);
             }
-            if (feat.rows>0)
+            if (feat.rows>0) {
+                if (fname=="vlad"||fname=="vladsift") {
+                    rootsift(feat);
+                }
                 bow_train->add(feat);
+            }
+
             fsize += feat.rows;
             cout << i << "\r";
             if (i%200==199)
                 cout << "cluster " << fname + " " << i << " " << id << " " << feat.size() << " " << fsize << endl;
         }
-        cout << bow_train->descriptorsCount() << " features." << endl;
+
+        cout << "clustering " << bow_train->descriptorsCount() << " features." << endl;
         {
             PROFILEX("bow:cluster")
             vocab = bow_train->cluster();
         }
+
         FileStorage fs2(fname+".voc.yml", 1+FileStorage::BASE64);
         fs2 << "voc" << vocab;
         fs2.release();
     } else {
-        fs["voc"]>> vocab;
+        fs["voc"] >> vocab;
         fs.release();
     }
     cerr << "bow vocab: " << vocab.size() << endl;
     bow_extract->setVocabulary(vocab);
 
     int fsize=0;
-    Mat trainData, indices;;
+    Mat trainData, indices, tests;;
 
+    //vector<String> vec_cls;
+    //utils::fs::glob(datapath, "",vec_cls,false,true);
+
+    int TST = fn.size() / ncimages;
     FileStorage fs1(fname+".dat.yml", 0);
     if (!fs1.isOpened()) {
-        for (int i=0; i<ncimages; i++) {
+        for (int i=0; i<fn.size(); i++) {
             PROFILEX("train data")
-            int id = theRNG().uniform(0,fn.size());
+
+            int id = i; //theRNG().uniform(0,fn.size());
+            String cat = category(fn[id],datapath.size()+1);
+            if (cat == "BACKGROUND_Google") continue;
+
             Mat img = getImg(fn[id],fname);
             if (img.empty()) continue;
-            Mat feat = fname =="vlad" ? vlad_feature(det, matcher, vocab, img) : bow_feature(det, bow_extract, img);
+
+            Mat feat = (fname =="vlad"||fname=="vladsift") ? vlad_feature(det, matcher, vocab, img) : bow_feature(det, bow_extract, img);
             if (feat.rows) {
-                trainData.push_back(feat);
-                indices.push_back(id);
+                if (i%TST == 0) {
+                    tests.push_back(id);
+                } else {
+                    trainData.push_back(feat);
+                    indices.push_back(id);
+                }
             }
             fsize += feat.rows;
             cout << i << "\r";
             if (i%100==99)
-                cout << "train " << i << " " << id << " " << feat.size() << " " << fsize << endl;
+                cout << "train " << fname << " " << id << " " << feat.size() << " " << fsize << endl;
         }
         FileStorage fs2(fname+".dat.yml", 1+FileStorage::BASE64);
         fs2 << "dat" << trainData;
         fs2 << "ind" << indices;
+        fs2 << "tst" << tests;
         fs2.release();
     } else {
-        fs1["dat"]>> trainData;
-        fs1["ind"]>> indices;
+        fs1["dat"] >> trainData;
+        fs1["ind"] >> indices;
+        fs1["tst"] >> tests;
         fs1.release();
     }
     cout << "train " << fname << " " << trainData.size() << " " << trainData.type() << endl;
@@ -200,31 +261,47 @@ int main( int argc, char ** argv ) {
     }
 
     int K=5;
+    float correct=0; int ntests=0;
     cv::flann::SearchParams params;
-    for (int i=0; i<30; i++) {
-        int idx = theRNG().uniform(0,indices.rows);
-        int id = indices.at<int>(idx);
+    for (int i=0; i<tests.rows; i++) {
+        int id = tests.at<int>(i);//theRNG().uniform(0,indices.rows);
+        //int id = indices.at<int>(idx);
+        String cat = category(fn[id], datapath.size() + 1) ;
+            if (cat == "BACKGROUND_Google") continue;
+        if (cat == "faces_easy") cat = "faces";
+        cout << "test " << cat;
+
         Mat org = getImg(fn[id],fname);
         if (org.empty()) continue;
-        cout << "test ";
 
-        Mat feat = fname =="vlad" ? vlad_feature(det, matcher, vocab, org) : bow_feature(det, bow_extract, org);
+        Mat feat = (fname =="vlad"||fname=="vladsift") ? vlad_feature(det, matcher, vocab, org) : bow_feature(det, bow_extract, org);
         if (feat.empty()) continue;
         cv::Mat dists;
         cv::Mat found;
         index->knnSearch(feat, found, dists, K, params);
+        cout << " " << found << endl;
+
         Mat res;
         resize(org,res,Size(240,160));
         for (int j=1; j<K; j++) {
-            int id = indices.at<int>(found.at<int>(j));
+            int f = found.at<int>(0,j);
+            int id = indices.at<int>(f);
             Mat img = imread(fn[id],fname!="lucid"?0:1);
             if (img.empty()) continue;
+            String cat2 = category(fn[id], datapath.size() + 1) ;
+            if (cat2=="faces_easy") cat2="faces";
+            bool ok = (cat == cat2);
+            cout << "   " << ok << " " << cat2 << endl;
+            correct += ok;
+            ntests ++;
             resize(img,img,Size(240,160));
             hconcat(res,img,res);
         }
-        cout << " " << found << endl;
         imshow("I",res);
-        waitKey();
+        waitKey(3000);
+
+        float acc = correct / ntests;
+        cout << "final " << correct << " " << ntests << " " << acc << endl;
     }
     return 0;
 }
