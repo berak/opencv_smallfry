83f6c52a095dc7c41362c7604eefe5fa8add5586
berak <px1704@web.de>
2018-12-26 14:21:11 +0100

    fix tabs

--- java_dnn/FaceRecognition.java
+++ java_dnn/FaceRecognition.java
@@ -4,11 +4,11 @@ import org.opencv.imgcodecs.*;
 import org.opencv.imgproc.*;
 
 public class FaceRecognition {
-	public static Mat process(Net net, Mat img) {
+    public static Mat process(Net net, Mat img) {
         Mat inputBlob = Dnn.blobFromImage(img, 1./255, new Size(96,96), new Scalar(0,0,0), true, false);
         net.setInput(inputBlob);
         return net.forward().clone();
-	}
+    }
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
@@ -17,6 +17,6 @@ public class FaceRecognition {
         Mat feature2 = process(net, Imgcodecs.imread("C:/data/faces/lfw40_crop/Abdullah_Gul_0007.jpg")); // your data here !
         double dist  = Core.norm(feature1,  feature2);
         if (dist < 0.6)
-        	System.out.println("SAME !");
+            System.out.println("SAME !");
     }
 }
